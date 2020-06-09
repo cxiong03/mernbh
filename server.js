c8e2eb84963dd16958b14f235c89ddd35e240853
Chang <cnxiong03@gmail.com>
2020-06-09 17:08:51 -0400

    server setup and listening to port

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
