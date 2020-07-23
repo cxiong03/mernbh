a12513ed723029c270e1bddffd582f98748d4a82
Chang <cnxiong03@gmail.com>
2020-07-22 22:27:31 -0400

    get routes and put routes done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,65 +1,166 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-const Post = require('../../models/Post');
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-// @route   GET api/posts/test
-// @desc    Test route
-// @access  Public
-router.get('/test', (req, res) => { 
-    res.json({ message: 'This is the test route' });
+const postValidator = [
+  check("author", "Author is required").notEmpty(),
+  check("skillLevel", "Skill Level is required").notEmpty(),
+  check("skillLevel", "Select from dropdown").isIn([
+    "Beginner",
+    "Intermediate",
+    "Advanced",
+    "Associate",
+    "Junior",
+    "Senior",
+    "Lead",
+  ]),
+  check("title", "Title is required").notEmpty(),
+  check("link", "A useable link is required").notEmpty(),
+  check("link", "Valid URL Required!").isURL(),
+  check("resourceType", "A Valid Resource Type is required ").notEmpty(),
+  check("resourceType", "Select from dropdown").isIn([
+    "Article",
+    "Video",
+    "SlideShow",
+    "Book",
+    "eBook",
+    "PDF",
+    "PodCast",
+    "Website",
+    "Newsletter",
+    "Blog",
+    "Other",
+  ]),
+  check("cost", "Cost is required").notEmpty(),
+  check("cost", "A valid number is required").isNumeric(),
+  check("publishedAt", "Invalid Date").optional().isISO8601(),
+  check("videoLength", "Length of Video must be in whole minutes")
+    .optional()
+    .isInt(),
+  check("timeToComplete", "Time it took to complete must be a number")
+    .optional()
+    .isNumeric(),
+];
+
+// @route		Get api/posts/test
+// @desc		Test route
+// @access	Public
+router.get("/test", (req, res) => {
+  res.json({ message: "This is the test route" });
 });
 
-// @route   POST api/posts
-// @desc    create a new post
-// @access  Private
-router.post('/', auth, [
-    check('author', 'Author is required').notEmpty(),
-    check('skillLevel', 'Skill Level is required').notEmpty(),
-    check('skillLevel', 'Select from dropdown').isIn([
-        'Beginner',
-        'Intermediate',
-        'Advanced',
-        'Associate',
-        'Junior',
-        'Senior',
-        'Lead',
-    ]),
-    check('title', 'Title is required').notEmpty(),
-    check('link', 'A link is required').notEmpty(),
-    check('link', 'Valid URL is required').isURL(),
-    check('resourceType', 'Valid resource is required').notEmpty(),
-    check('resourceType', 'Select from dropdown').isIn([
-        'Article',
-        'Video',
-        'Slideshow',
-        'Book',
-        'eBook',
-        'PDF',
-        'Podcast',
-        'Website',
-        'Newsletter',
-        'Blog',
-        'Other'
-    ]),
-    check('cost', 'Cost is required').notEmpty(),
-    check('cost', 'A valid number is required').isNumeric(),
-    check('publishedAt', 'Date is required').optional().isISO8601(),
-    check('videoLength', 'Video length must be a number').optional().isNumeric(),
-    check('timeToComplete', 'Time to complete must be a number').optional().isNumeric()
+// @route		POST api/posts
+// @desc		create a new post
+// @access	private
+router.post("/", auth, [...postValidator], async (req, res) => {
+  console.log("hello");
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+  //David
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    const profileId = profile._id;
+
+    // find profile with userId so we can extract the profile._id
+    // attach the profileId to the postData
+    const postData = { ...req.body };
+    postData.poster = profileId;
+    // create a new post
+
+    const post = await Post.create(postData);
+    res.json(post);
+    // respond with new post
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// @route		GET api/posts/
+// @desc		Get all posts
+// @access	public
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find().populate("poster", "name Avatar");
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// @route		GET api/posts/:id
+// @desc		get a post based on id number
+// @access	public
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).populate(
+      "poster",
+      "name Avatar"
+    );
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// @route		PUT api/posts/:id
+// @desc		update an existing post based on id
+// @access	owner
+router.put("/:id", auth, [...postValidator], async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    if (!profile) {
+      res.status(403).json({ msg: "no profile" });
     }
-    res.json(req.body);
+    const postData = { ...req.body };
+
+    const post = await Post.findOneAndUpdate(
+      { _id: req.params.id, poster: profile._id },
+      postData,
+      { new: true }
+    );
+
+    if (!post) {
+      res.status(403).json({ msg: "Unable to update" });
+      // optional check if post exists. if it does respond with 401 unauthorized. if it doesn't respond with 404 not found.
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
-// videoLength => optional => number
-// timeToComplete => optional => number
+// router.put('/:id', auth, async (req, res) => {
+//   try {
+//     const post = await Post.findById(req.params.id);
+//     if(!post) {
+//       return res.status(404).json({ msg: 'Post not found' });
+//     }
+//     const profile = await Profile.findOne({user: req.user.id});
+//     if(profile._id !== post.poster) {
+//       return res.status(401).json({ msg: 'User not authorized'});
+//     }
+//     post = { ...post, ...req.body };
+
+//     post.save();
+//     res.json(post);
+
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json(error);
+//   }
+// });
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,105 +1,107 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
-const isEmpty = require('../../utils/isEmpty');
-const config = require('../../config');
+const isEmpty = require("../../utils/isEmpty");
+const config = require("../../config");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 //@route POST api/users
 //@desc  create new user
 //@access public
 router.post(
-    '/',
-    [
-        check('email', 'Email Required').not().isEmpty(),
-        check('email', 'Valid Email Required').isEmail(),
-        check('password', 'Please enter a password with at least 6 characters.').isLength({ min: 6 }),
-    ], 
-    async (req, res) => {
-        const errors = validationResult(req);
-        
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
+  "/",
+  [
+    check("email", "Email Required").not().isEmpty(),
+    check("email", "Valid Email Required").isEmail(),
+    check(
+      "password",
+      "Please enter a password with at least 6 characters."
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-        const { email, password } = req.body;
-        const userData = {email: email.toLowerCase(), password };
-        // const userData = {
-        //     email: req.body.email,
-        //     password: req.body.password
-        // }
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-        try {
-            const salt = await bcrypt.genSalt(10);
-            userData.password = await bcrypt.hash(userData.password, salt);
+    const { email, password } = req.body;
+    const userData = { email: email.toLowerCase(), password };
+    // const userData = {
+    //     email: req.body.email,
+    //     password: req.body.password
+    // }
 
-            // create the code that will check if the email is alrady inuse before creating the new user.
-            const existing = await User.findOne({ email: userData.email });
+    try {
+      const salt = await bcrypt.genSalt(10);
+      userData.password = await bcrypt.hash(userData.password, salt);
 
-            if (!isEmpty(existing)) {
-                return res.status(400).json({  email: 'Email exists' });
-            }
-            const user = await User.create(userData);
+      // create the code that will check if the email is alrady inuse before creating the new user.
+      const existing = await User.findOne({ email: userData.email });
 
-            res.json(user);
-        } catch(error) {
-            console.error(error);
-            res.status(500).json(error);
-        }
-});
+      if (!isEmpty(existing)) {
+        return res.status(400).json({ email: "Email exists" });
+      }
+      const user = await User.create(userData);
+
+      res.json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  }
+);
 
 // @route  PUT api/users/
 // @desc   login a user
 // @access public
 router.put(
-    '/',
-    [
-        check('email', 'Email Required').not().isEmpty(),
-        check('email', 'Valid Email Required').isEmail(),
-        check('password', 'Password is required').not().isEmpty(),
-    ], 
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.stauts(400).json({ errors: errors.array ()
-            });
-        }
-
-        try {
-            const user = await User.findOne({ email: req.body.email.toLowerCase() });
-
-            if (isEmpty(user)) {
-                return req.status(404).json({ email: 'Email not found' });
-            }
-
-            const isMatch = await bcrypt.compare(req.body.password, user.password);
-
-            if (!isMatch) {
-                return req.status(401).json({ password: 'Incorrect password'});
-            }
-
-            // Validated! Challenge create the token and return it to the user.
-
-            User.findByIdAndUpdate(user.id, { lastLogin: Date.now() });
-
-            const payload = {
-                id: user.id,
-                email: user.email
-                //iat: Date
-            };
-
-            const token = jwt.sign(payload, config.secretOrKey, {});
-
-            return res.json(token);
-            
-        } catch (error) {
-            console.error(error);
-            res.status(500).json(error);
-        }
+  "/",
+  [
+    check("email", "Email Required").not().isEmpty(),
+    check("email", "Valid Email Required").isEmail(),
+    check("password", "Password is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.stauts(400).json({ errors: errors.array() });
     }
+
+    try {
+      const user = await User.findOne({ email: req.body.email.toLowerCase() });
+
+      if (isEmpty(user)) {
+        return req.status(404).json({ email: "Email not found" });
+      }
+
+      const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+      if (!isMatch) {
+        return req.status(401).json({ password: "Incorrect password" });
+      }
+
+      // Validated! Challenge create the token and return it to the user.
+
+      User.findByIdAndUpdate(user.id, { lastLogin: Date.now() });
+
+      const payload = {
+        id: user.id,
+        // email: user.email
+        //iat: Date
+      };
+
+      const token = jwt.sign(payload, config.secretOrKey, {});
+
+      return res.json(token);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  }
 );
 
 module.exports = router;
