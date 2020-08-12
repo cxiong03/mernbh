a360f06a876ecc4b738a29f78d3561cb3dcfad28
Chang <cnxiong03@gmail.com>
2020-08-11 20:56:50 -0400

    delete comment route done

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -32,3 +32,41 @@ router.post("/:postID", auth, async (req, res) => {
 // @route  DELETE api/posts/comments/:postID/:commentID
 // @desc   find and delete a specific comment from a post
 // @access owner
+router.delete("/:postID/:commentID", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postID);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.commentID
+    );
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment not found" });
+    }
+
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    if (profile.id !== post.comments[index].profile) {
+      return res.status(401).json({ msg: "Unauthorized" });
+    }
+    // post.comments.splice(index, 1);
+    // post.save();
+    // res.json(post.comments);
+    const updatedPost = await Post.findByIdAndUpdate(req.params.postID, {
+      $pull: { comments: { id: req.params.id, profile: profile.id } },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// epic: find and remove from a posts comment array the specific comment
+// and return the new comment array
+
+// check logged in
+// get the comment
+// check if user is owner of comment
+// delete the comment
+// return comment array to requester
